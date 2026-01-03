36c4d96341bba33eba7e6d6a197641dec07df0cc
AKR9634 <akhil6july2003@gmail.com>
2026-01-03 22:14:22 +0530

    OOPS revision

---
+++ OOPS.py
@@ -0,0 +1,32 @@
+class vehicle:
+
+    def __init__(self, name = "General vehicle"):
+        self.name = name
+
+    def drive():
+        pass
+
+
+class bike(vehicle):
+
+    def __init__(self, name = "B"):
+        super().__init__(name)
+
+    def drive(self):
+        print(f"Hey that's a {self.name} bike!!! \n")
+
+class car(vehicle):
+
+    def __init__(self, name = "C"):
+        super().__init__(name)
+
+    def drive(self):
+        print(f"Hey that's a {self.name} car!!! \n")
+        
+
+b = bike("Blaze Cruiser")
+
+c = car("Aston Martin Valhalla")
+
+b.drive()
+c.drive()
